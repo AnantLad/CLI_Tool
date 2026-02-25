7216a317ac624e92499f4574cb3b35296af298fc
AnantLad <anantlad018@gmail.com>
2026-02-25 11:56:10 +0530

    file organize

--- main.c
+++ main.c
@@ -1,59 +1,35 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include "tasks.h"
 
-#define TASK_FILE "tasks.txt"
-     
 int main(int argc, char *argv[]) {
 
     if (argc < 2) {
-        printf("Usage: devlog <command> [task]\n");
+        printf("Usage: devlog <add|list|done|delete>\n");
         return 1;
     }
 
     if (strcmp(argv[1], "add") == 0) {
-        if(argc < 3){
-            printf("Usage: devlog add <task description>\n");
+        add_task(argc, argv);
+    }
+    else if (strcmp(argv[1], "list") == 0) {
+        list_tasks();
+    }
+    else if (strcmp(argv[1], "done") == 0) {
+        if (argc < 3) {
+            printf("Usage: devlog done <task_number>\n");
             return 1;
         }
-    
-    FILE *file = fopen(TASK_FILE, "a");
-    if(file == NULL){
-        printf("Error opening file.\n");
-        return 1;
+        mark_done(atoi(argv[2]));
     }
-    
-    for(int i = 2; i < argc; i++){
-        fprintf(file, "%s", argv[i]);
-        if(i < argc - 1) fprintf(file, " ");
-    }
-    fprintf(file , "\n");
-    fclose(file);
-
-    printf("Task added successfully.\n");
-    }
-
-
-    else if (strcmp(argv[1], "list") == 0) {
-        FILE *file = fopen(TASK_FILE , "r");
-        if(file == NULL){
-            printf("No task found.\n");
-            return 0;
+    else if (strcmp(argv[1], "delete") == 0) {
+        if (argc < 3) {
+            printf("Usage: devlog delete <task_number>\n");
+            return 1;
         }
-
-        char line[256];
-        int count = 1;
-        while(fgets(line, sizeof(line), file)){
-            printf("%d. %s", count, line);
-            count++;
-        }
-        fclose(file);
+        delete_task(atoi(argv[2]));
     }
-
-    else if (strcmp(argv[1], "delete") == 0){
-
-        printf("Remove command detected\n");
-    }
-    
     else {
         printf("Unknown command\n");
     }
